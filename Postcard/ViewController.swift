9b70223d5b2f96cd2ccd8ad3918294593de6f4e2
vermau <vermau81@gmail.com>
2014-12-15 03:23:47 +1100

    First commit
    This is my first commit

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  PostCard
+//
+//  Created by Ujjwal Verma on 11/16/14.
+//  Copyright (c) 2014 iCoder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var lblMessage: UILabel!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtMessage: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnSendMailClicked(sender: UIButton) {
+        if((txtMessage.text != "") || (txtName.text != "")) {
+            lblMessage.hidden = false;
+            lblMessage.text = "Name: " + txtName.text + "\nMessage: " + txtMessage.text;
+            lblMessage.textColor = UIColor.brownColor();
+            
+            txtName.text = "";
+            txtMessage.text = "";
+        }else {
+            lblMessage.hidden = true;
+        }
+    }
+
+}
+
